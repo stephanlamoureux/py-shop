5cf69050ebc5dd82780b70b8e7945df48b8afc05
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-03-25 23:25:08 -0400

    small updates

--- store.py
+++ store.py
@@ -1,39 +1,64 @@
-freelancers = {'name':'freelancing Shop','brian': 70, 'black knight':20, 'biccus diccus':100, 'grim reaper':500, 'minstrel':-15}
-antiques = {'name':'Antique Shop','french castle':400, 'wooden grail':3, 'scythe':150, 'catapult':75, 'german joke':5}
-pet_shop = {'name':'Pet Shop','blue parrot':10, 'white rabbit':5, 'newt': 2}
+melee = {
+    "name": "Melee Shop",
+    "baseball bat": 70,
+    "butterfly knife": 20,
+    "scythe": 100,
+    "katana": 500,
+    "golf club": 15,
+}
+guns = {
+    "name": "Gun Store",
+    "rifle": 400,
+    "pellet gun": 3,
+    "pistol": 150,
+    "shotgun": 75,
+    "machine gun": 750,
+}
+tailor = {
+    "name": "Tactical Tailor",
+    "night vision": 100,
+    "kevlar helmet": 250,
+    "bullet proof vest": 400,
+    "assault gloves": 100,
+}
 
-purse = 1000
+credits = 10000
 cart = {}
 total = 0
-department_store = {**freelancers, **antiques, **pet_shop}
-department_store.pop('name')
+inventory = {**melee, **guns, **tailor}
+inventory.pop("name")
 
-print('------------------------')
-print(f'Starting inventory is: \n{sorted(department_store.items())}')
-print('------------------------')
+print("---------------------------")
+print(f'Starting Inventory: \n[{", ".join(list(sorted(inventory)))}]')
+print("---------------------------\n")
 
-for shop in (freelancers,antiques,pet_shop):
-    print(f'Welcome to the {shop["name"]}! What would you like to buy? ("exit" to leave store)')
-    del shop['name']
-    print(f'items: [{", ".join(list(shop))}]')
-    buy_item = input('> ').lower()
+for shop in (melee, guns, tailor):
+    print(
+        f'Welcome to the {shop["name"]}. What would you like to buy? ("exit" to leave):'
+    )
+    del shop["name"]
+    print(f'items: {", ".join(list(shop))}')
+    buy_item = input("> ").lower()
+    print("---------------------------\n")
 
-
-    print('------------------------')
-    if buy_item == 'exit' or buy_item not in shop:
+    if buy_item == "exit":
+        continue
+    elif buy_item not in shop:
         continue
     else:
-        cart.update({buy_item:shop.pop(buy_item)})
-        department_store.pop(buy_item)
+        cart.update({buy_item: shop.pop(buy_item)})
+        inventory.pop(buy_item)
         items_bought = ", ".join(list(cart.keys()))
         total += sum(cart.values())
 
-purse = purse - total
+credits = credits - total
 
 if len(cart) == 0:
     items_bought = "no items"
-print(f'You purchased {items_bought}, for {total} gold. You have {purse} gold remaining.')
+print(
+    f"You purchased {items_bought}, for {total} credits. You have {credits} gold remaining.\n\nPrepare for the horde!!!\n"
+)
 
-print('------------------------')
-print(f'Final inventory is: \n{sorted(department_store.items())}')
-print('------------------------')
+print("---------------------------")
+print(f'Final Inventory: \n[{", ".join(list(sorted(inventory)))}]')
+print("---------------------------")
